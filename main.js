e67fed3769a8cde8118949843a4a2483a9c11266
Firlana Saddam <firlana.saddam.20@student.se.ittelkom-sby.ac.id>
2023-01-13 15:06:32 +0700

    update page 1 & music

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 onload = () =>{
-        document.body.classList.remove("container");
+        document.body.classList.remove("container")
+
 };
+
+function play() {
+	const page1 = document.getElementById("page1")
+	const page2 = document.getElementById("page2")
+	const audio = document.getElementById("my_audio")
+	page1.style.display = "none"
+	page2.style.display = "flex"
+	audio.play()
+
+}
